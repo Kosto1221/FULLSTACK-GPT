34f212bd20e9fca7199aafd1eb1c0e8f8681997e
Kosto1221 <ostolany@gamil.com>
2024-09-09 03:34:59 +0900

    commit for submission

--- app.py
+++ app.py
@@ -10,12 +10,5 @@ st.markdown(
 # Hello!
 
 Welcome to my FullstackGPT Portfolio!
-
-Here are the apps I made:
-
-- [ ] [DocumentGPT](/DocumentGPT)
-- [ ] [PrivateGPT](/PrivateGPT)
-- [ ] [QuizGPT](/QuizGPT)
-- [ ] [SiteGPT](/SiteGPT)
 """
 )
